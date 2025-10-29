a319374dc78d1d9fa6f3ad81c0de7d36ca89494c
nofilafzal <nofilafzal.55@gmail.com>
2025-10-30 00:26:27 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print ("hello world")
+print ("python")
+name = "ali"
+print (name)
+age = 19
+print (age)
+alpha = ["ali",19,22.3 ,"car"]
+print (alpha)
--- mani.py
+++
@@ -1,4 +0,0 @@
-print ("hello world")
-print ("python")
-name = "ali"
-print (name)
